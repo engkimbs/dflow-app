96dfbe9eb8bdab70d536771c126e494bc3efef12
engkimbs <eng.kimbs@gmail.com>
2021-08-26 23:33:35 +0900

    updater added

--- src/background.js
+++ src/background.js
@@ -3,6 +3,7 @@
 import { app, protocol, BrowserWindow } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
+import { autoUpdater} from "electron-updater"
 
 const isDevelopment = process.env.NODE_ENV !== 'production'
 const path = require('path')
@@ -36,6 +37,8 @@ async function createWindow() {
     // Load the index.html when not in development
     win.loadURL('app://./index.html')
   }
+
+  autoUpdater.checkForUpdatesAndNotify();
 }
 
 // Quit when all windows are closed.
