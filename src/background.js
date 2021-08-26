0db099451e341bf428864e94de2ef4f751b222a7
engkimbs <eng.kimbs@gmail.com>
2021-08-26 21:51:44 +0900

    background.js previous version updated

--- src/background.js
+++ src/background.js
@@ -1,9 +1,17 @@
 'use strict'
 
-import { app, protocol, BrowserWindow } from 'electron'
+import {app, protocol, BrowserWindow, ipcMain} from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
-import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
+import installExtension, { VUEJS3_DEVTOOLS } from 'electron-devtools-installer'
+import { autoUpdater} from "electron-updater"
+
+const path = require('path')
 const isDevelopment = process.env.NODE_ENV !== 'production'
+
+autoUpdater.logger = require('electron-log');
+autoUpdater.logger.transports.file.level = 'info'
+
+let win;
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
@@ -12,15 +20,13 @@ protocol.registerSchemesAsPrivileged([
 
 async function createWindow() {
   // Create the browser window.
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      
-      // Use pluginOptions.nodeIntegration, leave this alone
-      // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
       nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
-      contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION
+      contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION,
+      preload: path.join(__dirname, "preload.js")
     }
   })
 
@@ -33,6 +39,13 @@ async function createWindow() {
     // Load the index.html when not in development
     win.loadURL('app://./index.html')
   }
+
+  sendStatusToWindow("wow")
+  autoUpdater.checkForUpdatesAndNotify();
+}
+
+function sendStatusToWindow(text) {
+  win.webContents.send('updater', text);
 }
 
 // Quit when all windows are closed.
@@ -47,7 +60,7 @@ app.on('window-all-closed', () => {
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  if (win.getAllWindows().length === 0) createWindow()
 })
 
 // This method will be called when Electron has finished
@@ -57,7 +70,7 @@ app.on('ready', async () => {
   if (isDevelopment && !process.env.IS_TEST) {
     // Install Vue Devtools
     try {
-      await installExtension(VUEJS_DEVTOOLS)
+      await installExtension(VUEJS3_DEVTOOLS)
     } catch (e) {
       console.error('Vue Devtools failed to install:', e.toString())
     }
@@ -79,3 +92,40 @@ if (isDevelopment) {
     })
   }
 }
+
+ipcMain.on("initialize", (event, arg) => {
+  console.log(event)
+  console.log(arg)
+  autoUpdater.checkForUpdatesAndNotify();
+})
+
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+})
+
+autoUpdater.on('update-available', (info) => {
+  sendStatusToWindow('Update available.');
+  console.log(info)
+})
+
+autoUpdater.on('update-not-available', (info) => {
+  sendStatusToWindow('Update not available.');
+  console.log(info)
+})
+
+autoUpdater.on('error', (err) => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
+})
+
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  sendStatusToWindow(log_message);
+})
+
+autoUpdater.on('update-downloaded', () => {
+  sendStatusToWindow('Update downloaded');
+
+  autoUpdater.quitAndInstall();
+});
